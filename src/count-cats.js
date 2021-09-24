7eec36f11a614a9ad379fd6e34da46e76bca1a10
Katiarynka <katia.tsygankova@gmail.com>
2021-09-24 21:15:12 +0300

    feat: solve the task Count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(matrix) {
+  function findTheCat(value) {
+    return value === '^^';
+  }
+
+  const reducer = (previousValue, currentValue) => previousValue + currentValue;
+
+  function findTheCats(array) {
+    return array.filter(findTheCat).length
+  }
+
+  let arrayWithCats = matrix.map(findTheCats);
+    if (arrayWithCats.length === 0) {
+        return 0;
+    }
+    return arrayWithCats.reduce(reducer)
 }
