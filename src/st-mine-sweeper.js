2f54371dc80393704796e1b43193dd1c7800bd69
Katiarynka <katia.tsygankova@gmail.com>
2021-09-25 23:00:23 +0300

    feat: solve minesweeper task

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,37 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper(matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] !== true) {
+
+        let countOfMines = 0;
+
+        if ((i - 1 >= 0) && (j - 1 >= 0) && matrix[i - 1][j - 1] === true) { countOfMines++ }
+        if ((i - 1 >= 0) && matrix[i - 1][j] === true) { countOfMines++ }
+        if ((i - 1 >= 0) && (j + 1 < matrix[i].length) && matrix[i - 1][j + 1] === true) { countOfMines++ }
+
+        if ((j - 1 >= 0) && matrix[i][j - 1] === true) { countOfMines++ }
+        if ((j + 1 < matrix[i].length) && matrix[i][j + 1] === true) { countOfMines++ }
+
+        if ((i + 1 < matrix.length) && (j - 1 >= 0) && matrix[i + 1][j - 1] === true) { countOfMines++ }
+        if ((i + 1 < matrix.length) && matrix[i + 1][j] === true) { countOfMines++ }
+        if ((i + 1 < matrix.length) && (j + 1 < matrix[i].length) && matrix[i + 1][j + 1] === true) { countOfMines++ }
+
+        matrix[i][j] = countOfMines;
+      }
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === true) {
+        matrix[i][j] = 1;
+      }
+    }
+  }
+
+  return matrix
+
 }
