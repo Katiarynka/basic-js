861e25a436f97e530548b2f5034dc2c0fba42212
Katiarynka <katia.tsygankova@gmail.com>
2021-10-01 00:58:03 +0300

    feat: solve vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,119 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(direct) {
+    this.direct = direct === undefined ? true : direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+
+    if (message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    let abc = [];
+    for (let i = 65; i <= 90; i++) {
+      abc.push(String.fromCharCode(i))
+    }
+
+    for (let i = 97; i <= 122; i++) {
+      abc.push(String.fromCharCode(i))
+    }
+
+    let keyString = '';
+    let j = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (!abc.includes(message[i])) {
+        keyString = `${keyString}${message[i]}`
+      }
+      else {
+        keyString = `${keyString}${key[j]}`
+        j++;
+        j = j === key.length ? 0 : j;
+      }
+
+    }
+
+    let result = [];
+
+    for (let i = 0; i < message.length; i++) {
+
+      if (!abc.includes(message[i])) {
+        result.push(message[i])
+      }
+      else {
+
+        let letterIndex = abc.indexOf(message[i]);
+        let shift = abc.indexOf(keyString[i]);
+
+        result.push(abc[(letterIndex + shift) % abc.length]);
+
+      }
+
+    }
+
+    if (!this.direct) {
+      return result.reverse().join('').toUpperCase()
+    }
+    return result.join('').toUpperCase();
+  }
+
+  decrypt(message, key) {
+
+    if (message === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    let abc = [];
+    for (let i = 65; i <= 90; i++) {
+      abc.push(String.fromCharCode(i))
+    }
+
+    for (let i = 97; i <= 122; i++) {
+      abc.push(String.fromCharCode(i))
+    }
+
+    let keyString = '';
+    let j = 0;
+
+    message.toLowerCase();
+
+    for (let i = 0; i < message.length; i++) {
+      if (!abc.includes(message[i])) {
+        keyString = `${keyString}${message[i]}`
+      }
+      else {
+        keyString = `${keyString}${key[j]}`
+        j++;
+        j = j === key.length ? 0 : j;
+      }
+
+    }
+
+    let result = [];
+
+    for (let i = 0; i < message.length; i++) {
+
+      if (!abc.includes(message[i])) {
+        result.push(message[i])
+      }
+      else {
+
+        let letterIndex = abc.indexOf(message[i]);
+        let shift = abc.indexOf(keyString[i]);
+
+        result.push(abc[(letterIndex - shift + abc.length) % abc.length]);
+
+      }
+
+    }
+
+    if (!this.direct) {
+      return result.reverse().join('').toUpperCase()
+    }
+    return result.join('').toUpperCase();
+
   }
 }
