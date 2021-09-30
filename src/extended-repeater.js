926578d4441dade6bb56cce3809976c3ec5480b8
Katiarynka <katia.tsygankova@gmail.com>
2021-09-30 22:21:04 +0300

    feat: solve extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,36 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  
+    let repeatTimes = options.repeatTimes !== undefined ? options.repeatTimes : 1;
+    let separator = options.separator !== undefined ? options.separator  : '+';
+     
+    let addition = options.addition !== undefined ? options.addition : '';
+    let additionRepeatTimes = options.additionRepeatTimes !== undefined ? options.additionRepeatTimes : 0;
+    let additionSeparator = options.additionSeparator !== undefined ? options.additionSeparator  : '|';
+    let resultAddition = '';
+    
+    if (additionRepeatTimes === 0 && addition != '') {additionRepeatTimes = 1}
+
+    for (let i = 1; i <= additionRepeatTimes; i++) {
+        
+        if (i === additionRepeatTimes) {additionSeparator = ''}
+
+        resultAddition = `${resultAddition}${addition}${additionSeparator}`;
+
+    }
+
+    let resultString = '';
+
+    for (let i = 1; i <= repeatTimes; i++) {
+
+        if (i === repeatTimes) {separator = ''}
+
+        resultString = `${resultString}${str}${resultAddition}${separator}`
+
+    }
+
+    //console.log(resultString)
+    return resultString;
 }
