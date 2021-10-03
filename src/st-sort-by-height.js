4aa0f644ef774d208ecb04cc5a8b87d9d285b805
Katiarynka <katia.tsygankova@gmail.com>
2021-10-03 22:14:32 +0300

    solve sort by bit task

--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -11,7 +11,30 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function sortByHeight(arr) {
+  
+  for (let i = arr.length - 2; i >= 0; i--) {
+    for (let j = 0; j <= i; j++) {
+        if (arr[j] !== -1) {
+            if (arr[j + 1] === -1) {
+                for (let k = j + 2; k <= i + 1; k++) {
+                    if (arr[k] !== -1) {
+                        if (arr[j] > arr[k]) {
+                            let temp = arr[j];
+                            arr[j] = arr[k];
+                            arr[k] = temp;
+                        }
+                        break
+                    }
+                }
+            }
+            else if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+return arr;
 }
